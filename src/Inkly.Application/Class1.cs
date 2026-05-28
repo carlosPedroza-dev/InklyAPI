3592a51b15608214483ce4b9acb59c44a795fc5b
carlosPedroza-dev <carlospedroza.dev@gmail.com>
2026-05-28 01:33:40 -0600

    chore: restucture solution to Clean Arquitecture [Domain,Application,Infrastructure,API,Tests]

---
+++ src/Inkly.Application/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Inkly.Application;
+
+public class Class1
+{
+
+}
---
+++ tests/Inkly.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Inkly.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
